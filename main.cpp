0f87f0173d4208e9b039dd0c40a2ea0467645e03
lukasbabuchna <lukas.babuchna@gmail.com>
2026-06-29 12:23:54 +0000

    Added hostname function and completed memory info function

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@
 #include <tuple>
 #include <regex>
 
-char *memoryFilePath = "/proc/meminfo";
-char *temperatureMemoryPath = "/sys/class/thermal/thermal_zone0/temp";
-
-int getTemperature()
+int getTemperature(std::string temperatureMemoryPath)
 {
     std::ifstream temperatureFile(temperatureMemoryPath);
 
@@ -28,13 +25,13 @@ int getTemperature()
     return temperature;
 }
 
-std::tuple<int, int> getMemory()
+std::tuple<int, int> getMemory(std::string memoryFilePath)
 {
     std::ifstream memoryFile(memoryFilePath);
 
     if (!memoryFile.is_open())
     {
-        std::cout << "Couldn't open the memory file \n";
+        std::cout << "Couldn't open the memory file" << "\n";
         return std::make_tuple(0, 0);
     }
 
@@ -44,12 +41,74 @@ std::tuple<int, int> getMemory()
     getline(memoryFile, rawTotal); // MemTotal:        8255888 kB
     getline(memoryFile, rawFree);  // MemFree:         6814080 kB
 
-    // TODO: use regex to extract ints or pass the whole string to Python to parse it
+    std::smatch match;
+    std::regex regex(R"(\d+)");
+
+    int totalMem;
+    int freeMem;
+
+    // Regex on total
+
+    if (std::regex_search(rawTotal, match, regex))
+    {
+        totalMem = std::stoi(match.str());
+    }
+    else
+    {
+        std::cout << "Could find number in total memory string" << "\n";
+        totalMem = 0;
+    }
+
+    // Regex on free
+
+    if (std::regex_search(rawFree, match, regex))
+    {
+        freeMem = std::stoi(match.str());
+    }
+    else
+    {
+        std::cout << "Could find number in total memory string" << "\n";
+        freeMem = 0;
+    }
+
+    // total memory , free memory
+
+    return std::tuple(totalMem, freeMem); // values in kB
+}
+
+std::string getHostname()
+{
+    std::ifstream hostnameFile("/etc/hostname");
+
+    std::string hostname;
+
+    if (!hostnameFile.is_open())
+    {
+        std::cout << "Couldn't open the memory file" << "\n";
+        return "Unknown";
+    }
+
+    getline(hostnameFile, hostname);
+
+    return hostname;
 }
 
 int main()
 {
 
-    std::cout << getTemperature() << "\n";
+    std::string memoryFilePath = "/proc/meminfo";
+    std::string temperatureMemoryPath = "/sys/class/thermal/thermal_zone0/temp";
+
+    std::cout << getTemperature(temperatureMemoryPath) << "\n";
+
+    std::tuple<int, int> memoryData = getMemory(memoryFilePath);
+
+    int totalMemory = std::get<0>(memoryData);
+    int freeMemory = std::get<1>(memoryData);
+
+    std::cout << totalMemory << "\n"
+              << freeMemory << "\n";
+
+    std::cout << getHostname() << "\n";
     return 0;
 }
