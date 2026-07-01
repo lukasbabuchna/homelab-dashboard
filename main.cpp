dee56ad68cfe12ae1bd64852a65b83e7a7ea761a
lukasbabuchna <lukas.babuchna@gmail.com>
2026-07-01 14:35:22 +0000

    Monitor load avarage and CPU cores

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include <stdio.h>
 #include <fstream>
@@ -8,6 +9,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <vector>
 
 #include <nlohmann/json.hpp>
 
@@ -83,7 +85,7 @@ std::tuple<int, int> getMemory(std::string memoryPath)
 
     // total memory , free memory
 
-    return std::tuple(totalMem, freeMem); // values in kB
+    return std::make_tuple(totalMem, freeMem); // values in kB
 }
 
 std::string getHostname(std::string hostnamePath)
@@ -105,6 +107,76 @@ std::string getHostname(std::string hostnamePath)
     return hostname;
 }
 
+std::tuple<float, float, float> getLoadAvarage(std::string loadAvaragePath)
+{
+    std::ifstream loadAvarageFileStream(loadAvaragePath);
+
+    if (!loadAvarageFileStream.is_open())
+    {
+        std::cout << "Couldn't open the load avarge file";
+        return std::make_tuple(0, 0, 0);
+    }
+
+    std::string rawLoadAvg;
+
+    getline(loadAvarageFileStream, rawLoadAvg); // 0.33 0.63 0.64 2/1673 16184
+
+    // tokenizing the output
+    std::vector<std::string> loadAvgTokens;
+    std::string temp;
+
+    rawLoadAvg = rawLoadAvg + " "; // the last token will be pushed to the vector
+
+    for (int i = 0; i < rawLoadAvg.size(); i++)
+    {
+        if (rawLoadAvg[i] == ' ')
+        {
+            loadAvgTokens.push_back(temp);
+            temp = "";
+            continue;
+        }
+
+        temp = temp + rawLoadAvg[i];
+    }
+
+    float load1 = std::stof(loadAvgTokens[0]);  // load in 1 minute
+    float load5 = std::stof(loadAvgTokens[1]);  // load in 5 minutes
+    float load15 = std::stof(loadAvgTokens[2]); // load in 15 minutes
+
+    return std::make_tuple(load1, load5, load15);
+}
+
+int getNumOfCPUCores(std::string numOfCPUCoresPath)
+{
+    std::ifstream numOfCPUCoresFileStream(numOfCPUCoresPath);
+
+    std::string rawCoresRange;
+
+    getline(numOfCPUCoresFileStream, rawCoresRange); // 0-5
+
+    // tokenizing raw output to get number of cores: [0, 5]
+
+    rawCoresRange = rawCoresRange + "-";
+    std::vector<int> coreRangeTokenized;
+    std::string temp;
+
+    for (int i = 0; i < rawCoresRange.size(); i++)
+    {
+        if (rawCoresRange[i] == '-')
+        {
+            coreRangeTokenized.push_back(std::stoi(temp));
+            temp = "";
+            continue;
+        }
+
+        temp = temp + rawCoresRange[i];
+    }
+
+    int numOfCores = (coreRangeTokenized[1] - coreRangeTokenized[0]) + 1;
+
+    return numOfCores;
+}
+
 int main()
 {
     // Adress to send data to (typically where your web dashboard runs)
@@ -115,6 +187,8 @@ int main()
     std::string hostnamePath = "/etc/hostname";
     std::string memoryPath = "/proc/meminfo";
     std::string temperaturePath = "/sys/class/thermal/thermal_zone0/temp";
+    std::string loadAvaragePath = "/proc/loadavg";
+    std::string numOfCPUCoresPath = "/sys/devices/system/cpu/online";
 
     // seting up socket
 
@@ -139,12 +213,23 @@ int main()
 
         std::string hostname = getHostname(hostnamePath);
 
+        std::tuple<float, float, float> avgLoads = getLoadAvarage(loadAvaragePath);
+        float load1 = std::get<0>(avgLoads);
+        float load5 = std::get<0>(avgLoads);
+        float load15 = std::get<0>(avgLoads);
+
+        int numOfCPUCores = getNumOfCPUCores(numOfCPUCoresPath);
+
         nlohmann::json jsonData = {{"temperature", temperature},
                                    {"totalMemory", totalMemory},
                                    {"freeMemory", freeMemory},
-                                   {"hostname", hostname}};
+                                   {"hostname", hostname},
+                                   {"load1", load1},
+                                   {"load5", load5},
+                                   {"load15", load15},
+                                   {"numOfCPUCores", numOfCPUCores}};
 
-        std::string jsonDataSerializated = jsonData.dump();
+        std::string jsonDataSerializated = jsonData.dump(); // turning JSON into string
 
         send(clientSocket, jsonDataSerializated.c_str(), strlen(jsonDataSerializated.c_str()), 0);
 
